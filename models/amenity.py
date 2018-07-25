bba1e809b67831b005b2e9c2b993fb4fb79d386c
Anthony la'O <anthonylao@live.com>
2018-07-25 12:06:36 +0000

    Revert back to previous files for checker test

--- models/amenity.py
+++ models/amenity.py
@@ -2,17 +2,11 @@
 '''
     Implementation of the Amenity class
 '''
-from os import getenv, environ
-from models.base_model import BaseModel, Base
-from models.place import place_amenity
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     '''
         Implementation for the Amenities.
     '''
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    name = ""
--- models/review.py
+++ models/review.py
@@ -2,18 +2,14 @@
 '''
     Implementation of the Review class
 '''
-from os import getenv, environ
+
 from models.base_model import BaseModel
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import Table, Float, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel):
     '''
         Implementation for the Review.
     '''
-    __tablename__ = 'reviews'
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    place_id = ""
+    user_id = ""
+    text = ""
