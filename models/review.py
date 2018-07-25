2783f21eafc8952d0049345d11f573bedc736caf
Anthony la'O <anthonylao@live.com>
2018-07-25 11:45:14 +0000

    Updated amenities, place, user, and review

--- models/review.py
+++ models/review.py
@@ -2,14 +2,18 @@
 '''
     Implementation of the Review class
 '''
-
+from os import getenv, environ
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import Table, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel):
     '''
         Implementation for the Review.
     '''
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
