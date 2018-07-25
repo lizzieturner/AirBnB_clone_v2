20965568c5e1c9d91de0f28699c13b184d1cc930
Lizzie Turner <hello@lizzieturner.com>
2018-07-25 20:07:29 +0000

    Update amenity tests

--- tests/test_models/test_amenity_model.py
+++ tests/test_models/test_amenity_model.py
@@ -9,6 +9,28 @@ from models.base_model import BaseModel
 from models.amenity import Amenity
 
 
+class TestDocsAndStyle(unittest.TestCase):
+    '''
+        Check for documentation and formatting
+    '''
+    def test_class_doc(self):
+        '''
+          Check documentation
+        '''
+        self.assertTrue(len(Amenity.__doc__) > 0)
+
+    def test_pep8(self):
+        '''
+            Check pep8 conformity
+        '''
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/amenity.py'
+        file2 = 'tests/test_models/test_amenity_model.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warning).")
+
+
 class TestAmenity(unittest.TestCase):
     '''
         Testing Amenity class
