fc24581de27d32bdcb69398a3ce15bdff1b0d3df
Lizzie Turner <hello@lizzieturner.com>
2018-07-25 04:13:42 +0000

    Update user class

--- models/user.py
+++ models/user.py
@@ -7,12 +7,12 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
     __tablename__ = "users"
-    email = Column(String(128). nullable=False)
+    email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
