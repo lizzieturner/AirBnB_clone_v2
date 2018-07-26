f5ad7fb8520ef4487a6dd892828bbc04943ab8a5
Lizzie Turner <hello@lizzieturner.com>
2018-07-26 04:58:40 +0000

    Update Review tests

--- tests/test_models/test_Review_model.py
+++ tests/test_models/test_Review_model.py
@@ -13,33 +13,39 @@ class TestReview(unittest.TestCase):
     '''
         Testing Review class
     '''
+    @classmethod
+    def setUpClass(cls):
+        '''
+            Intializes testing instance
+        '''
+        cls.rev = Review()
+        cls.rev.place_id = "3"
+        cls.rev.user_id = "10"
+        cls.rev.text = "text text text"
 
     def test_Review_inheritance(self):
         '''
             tests that the Review class Inherits from BaseModel
         '''
-        new_review = Review()
-        self.assertIsInstance(new_review, BaseModel)
+        self.assertIsInstance(self.rev, BaseModel)
 
     def test_Review_attributes(self):
         '''
             Test that Review class has place_id, user_id and text
             attributes.
         '''
-        new_review = Review()
-        self.assertTrue("place_id" in new_review.__dir__())
-        self.assertTrue("user_id" in new_review.__dir__())
-        self.assertTrue("text" in new_review.__dir__())
+        self.assertTrue("place_id" in self.rev.__dir__())
+        self.assertTrue("user_id" in self.rev.__dir__())
+        self.assertTrue("text" in self.rev.__dir__())
 
     def test_Review_attributes(self):
         '''
             Test that Review class has place_id, user_id and text
             attributes.
         '''
-        new_review = Review()
-        place_id = getattr(new_review, "place_id")
-        user_id = getattr(new_review, "user_id")
-        text = getattr(new_review, "text")
+        place_id = getattr(self.rev, "place_id")
+        user_id = getattr(self.rev, "user_id")
+        text = getattr(self.rev, "text")
         self.assertIsInstance(place_id, str)
         self.assertIsInstance(user_id, str)
         self.assertIsInstance(text, str)
