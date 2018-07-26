bfd8ad8b6b7f691cb4b3bc9c20062edfc7bfe7b0
Lizzie Turner <hello@lizzieturner.com>
2018-07-26 05:07:07 +0000

    Update amenity tests

--- tests/test_models/test_amenity_model.py
+++ tests/test_models/test_amenity_model.py
@@ -9,26 +9,15 @@ from models.base_model import BaseModel
 from models.amenity import Amenity
 
 
-class TestDocsAndStyle(unittest.TestCase):
+class TestAmenityDocs(unittest.TestCase):
     '''
-        Check for documentation and formatting
+        Check for documentation
     '''
     def test_class_doc(self):
         '''
-          Check documentation
+          Check class documentation
         '''
         self.assertTrue(len(Amenity.__doc__) > 0)
-
-    def test_pep8(self):
-        '''
-            Check pep8 conformity
-        '''
-        style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/amenity.py'
-        file2 = 'tests/test_models/test_amenity_model.py'
-        result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
 
 
 class TestAmenity(unittest.TestCase):
@@ -36,24 +25,29 @@ class TestAmenity(unittest.TestCase):
         Testing Amenity class
     '''
 
+    @classmethod
+    def setUpClass(cls):
+        '''
+            Initializes testing instance
+        '''
+        cls.amen = Amenity()
+        cls.amen.name = "Wifi"
+
     def test_Amenity_inheritence(self):
         '''
             tests that the Amenity class Inherits from BaseModel
         '''
-        new_amenity = Amenity()
-        self.assertIsInstance(new_amenity, BaseModel)
+        self.assertIsInstance(self.amen, BaseModel)
 
     def test_Amenity_attributes(self):
         '''
             Test that Amenity class had name attribute.
         '''
-        new_amenity = Amenity()
-        self.assertTrue("name" in new_amenity.__dir__())
+        self.assertTrue("name" in self.amen.__dir__())
 
     def test_Amenity_attribute_type(self):
         '''
             Test that Amenity class had name attribute's type.
         '''
-        new_amenity = Amenity()
-        name_value = getattr(new_amenity, "name")
+        name_value = getattr(self.amen, "name")
         self.assertIsInstance(name_value, str)
