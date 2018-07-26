b95b7aef9c7a15ef16c104018f285f704eb383bd
Lizzie Turner <hello@lizzieturner.com>
2018-07-26 05:39:30 +0000

    Update city tests

--- tests/test_models/test_city_model.py
+++ tests/test_models/test_city_model.py
@@ -13,31 +13,41 @@ class TestUser(unittest.TestCase):
     '''
         Testing User class
     '''
+    def test_class_docs(self):
+        '''
+           Test class documentation
+        '''
+        self.assertTrue(len(City.__doc__) > 0)
+
+    @classmethod
+    def setUpClass(cls):
+        '''
+            Initializing testing instance
+        '''
+        cls.new_city = City()
+        cls.new_city.state_id = "5"
+        cls.new_city.name = "San Francisco"
 
     def test_City_inheritance(self):
         '''
             tests that the City class Inherits from BaseModel
         '''
-        new_city = City()
-        self.assertIsInstance(new_city, BaseModel)
+        self.assertIsInstance(self.new_city, BaseModel)
 
     def test_User_attributes(self):
-        new_city = City()
-        self.assertTrue("state_id" in new_city.__dir__())
-        self.assertTrue("name" in new_city.__dir__())
+        self.assertTrue("state_id" in self.new_city.__dir__())
+        self.assertTrue("name" in self.new_city.__dir__())
 
     def test_type_name(self):
         '''
             Test the type of name
         '''
-        new_city = City()
-        name = getattr(new_city, "name")
+        name = getattr(self.new_city, "name")
         self.assertIsInstance(name, str)
 
     def test_type_name(self):
         '''
             Test the type of name
         '''
-        new_city = City()
-        name = getattr(new_city, "state_id")
+        name = getattr(self.new_city, "state_id")
         self.assertIsInstance(name, str)
