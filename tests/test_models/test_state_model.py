a3919845338a9709a76b8f792337700c22d6202f
Lizzie Turner <hello@lizzieturner.com>
2018-07-26 05:48:01 +0000

    Update State tests

--- tests/test_models/test_state_model.py
+++ tests/test_models/test_state_model.py
@@ -11,25 +11,36 @@ class TestState(unittest.TestCase):
     '''
         Test the State class.
     '''
+    @classmethod
+    def setUpClass(cls):
+        '''
+            Initializes testing instance
+        '''
+        cls.new_state = State()
+        cls.new_state.id = "5"
+        cls.new_state.name = "California"
+
+    def test_class_doc(self):
+        '''
+            Test class documentation
+        '''
+        self.assertTrue(len(State.__doc__) > 0)
 
     def test_State_inheritence(self):
         '''
             Test that State class inherits from BaseModel.
         '''
-        new_state = State()
-        self.assertIsInstance(new_state, BaseModel)
+        self.assertIsInstance(self.new_state, BaseModel)
 
     def test_State_attributes(self):
         '''
             Test that State class contains the attribute `name`.
         '''
-        new_state = State()
-        self.assertTrue("name" in new_state.__dir__())
+        self.assertTrue("name" in self.new_state.__dir__())
 
     def test_State_attributes_type(self):
         '''
             Test that State class attribute name is class type str.
         '''
-        new_state = State()
-        name = getattr(new_state, "name")
+        name = getattr(self.new_state, "name")
         self.assertIsInstance(name, str)
