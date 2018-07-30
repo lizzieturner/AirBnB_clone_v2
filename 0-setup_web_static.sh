5337f077b4e4c93c199f57c385eb6c1c5952148e
Lizzie Turner <hello@lizzieturner.com>
2018-07-30 21:54:42 +0000

    Fix filepath typo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,11 @@
 # sets up web servers for deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static_releases/test /data/web_static/shared
-echo "this is a test" | sudo tee /data/web_static_releases/test/index.html
-sudo ln -sf /data/web_static_releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "this is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-msg="location /hbnb_static {\nalias /data/web_static/current;\n}\n"
+msg="location /hbnb_static/ {\nalias /data/web_static/current;\n}"
 sudo sed -i "40i $msg" /etc/nginx/sites-enabled/default
 sudo service nginx restart
