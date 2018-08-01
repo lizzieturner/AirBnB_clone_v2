0fe654c0bb32d5d1c6d84caefc2ce6696fe64dcd
Lizzie Turner <hello@lizzieturner.com>
2018-08-01 22:55:53 +0000

    Fix typo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 ''' contains the function do_deploy '''
-from fabric.api import env, sudo, path, run
+from fabric.api import env, sudo, run, put
 from os.path import exists
 env.hosts = ['35.237.244.23', '35.196.221.206']
 env.user = 'ubuntu'
@@ -11,17 +11,17 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     file_name = archive_path.split('/')[-1]
-    tmp = '/tmp/' + file_name
-    new = '/data/web_static/release/' + file_name.split('.')[0]
+    new = '/data/web_static/release/' + \
+          "{}".format(file_name.split('.')[0])
     current = 'data/web_static/current'
     try:
-        put(archive_path, tmp)
-        run('sudo mkdir -p {}'.format(new))
-        run('sudo tar -xzf {} -C []'.format(tmp, new))
-        run('sudo rm {}'.format(tmp))
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p {}/'.format(new))
+        run('sudo tar -xzf {} -C {}/'.format(tmp, new))
+        run('sudo rm /tmp/{}'.format(file_name))
         run('sudo mv {}/web_static {}/'.format(new))
         run('sudo rm -rf {}'.format(current))
-        run('sudo ln -s {} {}'.format(new, current))
+        run('sudo ln -s {}/ {}'.format(new, current))
         return True
     except:
         return False
