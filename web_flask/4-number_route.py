d66cdc0330f446d554c7bf8436fa4ae04611716c
Lizzie Turner <hello@lizzieturner.com>
2018-08-15 22:04:19 +0000

    Create 4-number_route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+''' starts a Flask web application  '''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def slash():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def slash_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def slash_c(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def slash_python(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def slash_number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run()
